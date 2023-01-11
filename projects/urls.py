e7c754085e84018380194e2e290e116324524b04
Kim Yoo <kimyoo04eco@naver.com>
2023-01-12 04:21:02 +0900

    Implement projects PUT, DELETE API

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ProjectCreateAPIView.as_view()),
-    path('<int:pk>/', views.ProjectDetailAPIView.as_view())
+    path('<int:pk>/', views.ProjectDetailAPIView.as_view()),
+    path('<int:pk>/update/', views.ProjectUpdateAPIView.as_view()),
+    path('<int:pk>/delete/', views.ProjectDestroyAPIView.as_view())
 ]
--- projects/views.py
+++ projects/views.py
@@ -5,6 +5,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status, generics
 
+
 class ProjectDetailAPIView(generics.RetrieveAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
@@ -21,3 +22,22 @@ class ProjectCreateAPIView(generics.ListCreateAPIView):
             content = title
         serializer.save(content=content)
 
+
+class ProjectUpdateAPIView(generics.UpdateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+
+
+class ProjectDestroyAPIView(generics.DestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        super().perform_destroy(instance)
