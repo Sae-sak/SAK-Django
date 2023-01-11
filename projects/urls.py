753d1aaa96fd487eda0650f47b9e4dc4bc24b0bb
Kim Yoo <kimyoo04eco@naver.com>
2023-01-12 04:17:18 +0900

    Implement class based view GET POST API

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.project_list),
+    path('', views.ProjectCreateAPIView.as_view()),
     path('<int:pk>/', views.ProjectDetailAPIView.as_view())
 ]
--- projects/views.py
+++ projects/views.py
@@ -5,20 +5,19 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status, generics
 
-@api_view(['GET', 'POST'])
-def project_list(request, *args, **kwargs):
-
-    if request.method == 'GET':
-        projects = Project.objects.all().values()
-        serializer = ProjectSerializer(projects, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    if request.method == 'POST':
-        serializer = ProjectSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(data=request.data, status=status.HTTP_201_CREATED)
-
 class ProjectDetailAPIView(generics.RetrieveAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+
+class ProjectCreateAPIView(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
